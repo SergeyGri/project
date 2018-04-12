1483095b664b2eace2bbc039ea9c26b7e0e4747d
SergeyGri <sergegrecha@gmail.com>
2018-04-12 14:00:16 +0300

    buy-menu

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -2,6 +2,16 @@ jQuery(document).ready(function(){
     jQuery('.owl-carousel').owlCarousel({
         items: 1,
         dots: true,
-        loop: true
+        responsive:{
+            0:{
+                items:3
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:1
+            }
+        }
     });
 });
