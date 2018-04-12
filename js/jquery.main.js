f4223e4d94b253371d9bb771a1b72ce7c4999b85
SergeyGri <sergegrecha@gmail.com>
2018-04-12 16:03:29 +0300

    carousel-3

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,17 +1,9 @@
 jQuery(document).ready(function(){
-    jQuery('.owl-carousel').owlCarousel({
+    jQuery('.owl-carousel-1').owlCarousel({
         items: 1,
-        dots: true,
-        responsive:{
-            0:{
-                items:3
-            },
-            600:{
-                items:2
-            },
-            1000:{
-                items:1
-            }
-        }
+        dots: true
+    });
+    jQuery('.owl-carousel-3').owlCarousel({
+        items: 3,
     });
 });
