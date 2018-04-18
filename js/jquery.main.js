1ce738b3584188da80f178717f714bd014792731
SergeyGri <sergegrecha@gmail.com>
2018-04-18 14:06:19 +0300

    forgot your password dialog

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -6,4 +6,13 @@ jQuery(document).ready(function(){
     jQuery('.owl-carousel-3').owlCarousel({
         items: 3,
     });
+
+    var dialog = jQuery( "#dialog" ).dialog({
+        autoOpen: false
+    });
+    jQuery(".forgot-password").on( "click", function(e) {
+        e.preventDefault();
+        dialog.dialog( "open" );
+    });
+
 });
