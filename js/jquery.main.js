a0b4857f7e62809be18a9e60713f60b759d74162
SergeyGri <sergegrecha@gmail.com>
2018-04-23 18:21:21 +0300

    spoiler JS

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -13,7 +13,26 @@ jQuery(document).ready(function(){
         e.preventDefault();
         dialog.dialog( "open" );
     });
+    jQuery( function() {
+        function runEffect() {
+            var selectedEffect = jQuery( "#effectTypes" ).val();
+            var options = {};
+            if ( selectedEffect === "slide" ) {
+                options = { percent: 50 };
+            } else if ( selectedEffect === "size" ) {
+                options = { to: { width: 200, height: 60 } };
+            }
+            jQuery( ".toggler" ).toggle( selectedEffect, options, 500 );
+        }
+        jQuery( ".spoiler-button" ).on( "click", function() {
+            runEffect();
+        });
+    } );
 
+    // jQuery(".spoiler-button").click(function(){
+    //     jQuery(".toggler").slideToggle("slow");
+    //     jQuery(this).toggleClass("active"); return false;
+    // });
 });
 
 
